6d57a237953b9d1c9a2fd56c5ccd65a953748787
Mario Felipe de Oliveira Farhat <50421880+mariofelipefarhat@users.noreply.github.com>
2023-07-31 10:20:52 -0300

    added: infrastructure dependency injection

--- API/Coodesh.API/Program.cs
+++ API/Coodesh.API/Program.cs
@@ -3,40 +3,21 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
-var dbHost = Environment.GetEnvironmentVariable("DB_HOST") ?? "coodesh.sqlserver";
-var dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "CoodeshDB";
-var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD") ?? "password@12345#";
-var dbPort = Environment.GetEnvironmentVariable("DB_PORT") ?? "1433";
-
-var connectionString = $"Server={dbHost},{dbPort};Initial Catalog={dbName};User ID=SA;Password={dbPassword};MultipleActiveResultSets=true;TrustServerCertificate=true";
-
-builder.Services.AddDbContext<TransactionContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddInfrastructurePersistence();
 
 var app = builder.Build();
 
-using IServiceScope dbServiceScope = app.Services.CreateScope();
-TransactionContext? ctx = dbServiceScope.ServiceProvider.GetService<TransactionContext>() ?? null;
-ctx?.Database.Migrate();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.Services.CreateScope().ServiceProvider.GetService<TransactionContext>()?.Database.Migrate();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
---
+++ API/Coodesh.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using Coodesh.Infrastructure.Persistence.Product;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Coodesh.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructurePersistence(this IServiceCollection services)
+        {
+            try
+            {
+                var dbHost = Environment.GetEnvironmentVariable("DB_HOST") ?? "coodesh.sqlserver";
+                var dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "CoodeshDB";
+                var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD") ?? "password@12345#";
+                var dbPort = Environment.GetEnvironmentVariable("DB_PORT") ?? "1433";
+
+                var connectionString = $"Server={dbHost},{dbPort};Initial Catalog={dbName};User ID=SA;Password={dbPassword};MultipleActiveResultSets=true;TrustServerCertificate=true";
+
+                services.AddDbContext<TransactionContext>(options => options.UseSqlServer(connectionString));
+                services.AddScoped<ITransactionRepository, TransactionRepository>();
+                
+            }
+            catch(Exception e)
+            {
+
+            }
+            return services;
+        }
+    }
+}
